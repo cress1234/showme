1c71a8df2a008520b8edf386c22333b65e10c430
Cressida Cleland <cress1234@github.com>
2019-08-30 20:15:38 +0100

    update

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-let tv_array = ["Peep Show","Arrested Development","Mindhunter","Glee","That Mitchell and Webb Look","Extras","How I Met Your Mother","It's Always Sunny In Philadelphia"]
+let tv_array = ["Peep Show","Arrested Development","Mindhunter",
+				"Glee","That Mitchell and Webb Look","Extras",
+				"How I Met Your Mother",
+				"It's Always Sunny In Philadelphia"]
 let textbox = document.getElementById("text-box")
 let initial_html = textbox.innerHTML
 let l = tv_array.length
