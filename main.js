c51f0b5562008657835197f37bc58db6fc70bfe5
Harry Cooke <hcooke006@aol.com>
2019-08-30 21:32:07 +0100

    Modified output div to also act as input

--- main.js
+++ main.js
@@ -5,9 +5,22 @@ let textbox = document.getElementById("output")
 let initial_html = textbox.innerHTML
 let l = tv_array.length
 
-function getShow(){
+let input_is_output = false
+
+function getShow()
+{
+    input_is_output = true
 	let s = Math.floor(Math.random()*l)
-	textbox.innerHTML =/* initial_html + "<br>" +*/ tv_array[s]
+	textbox.value=/* initial_html + "<br>" +*/ tv_array[s]
+}
+
+function clearInput()
+{
+    if (input_is_output)
+    {
+        textbox.value = ""
+        input_is_output = false
+    }
 }
 
 
