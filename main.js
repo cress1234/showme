bc7780b253fa05b086b5080b314d7497c01af6ec
Cressida Cleland <cress1234@github.com>
2019-08-30 18:53:26 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let tv_array = ["Peep Show","Arrested Development","Mindhunter","Glee","That Mitchell and Webb Look","Extras","How I Met Your Mother","It's Always Sunny In Philadelphia"]
+let textbox = document.getElementById("text-box")
+let initial_html = textbox.innerHTML
+let l = tv_array.length
+
+function getShow(){
+	let s = Math.floor(Math.random()*l)
+	textbox.innerHTML = initial_html + "<br>" + tv_array[s]
+}
+
+
