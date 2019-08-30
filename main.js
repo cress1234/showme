27dce3210224ca3a3f317da5a8df56e82d9e2cc6
Harry Cooke <hcooke006@aol.com>
2019-08-30 20:08:44 +0100

    structural formatting

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-let tv_array = ["Peep Show","Arrested Development","Mindhunter","Glee","That Mitchell and Webb Look","Extras","How I Met Your Mother","It's Always Sunny In Philadelphia"]
-let textbox = document.getElementById("text-box")
+let tv_array = ["Peep Show","Arrested Development","Mindhunter","Glee",
+                "That Mitchell and Webb Look",
+                "Extras","How I Met Your Mother","It's Always Sunny In Philadelphia"]
+let textbox = document.getElementById("output")
 let initial_html = textbox.innerHTML
 let l = tv_array.length
 
 function getShow(){
 	let s = Math.floor(Math.random()*l)
-	textbox.innerHTML = initial_html + "<br>" + tv_array[s]
+	textbox.innerHTML =/* initial_html + "<br>" +*/ tv_array[s]
 }
 
 
